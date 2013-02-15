5194ec0910648b8faf0ac1990578c8145d526dba
Bernd Weber <bernd@copperfroghosting.com>
2013-02-15 11:03:29 -0800

    Let's make sure the FQDN and hostname are defined for the set IP.

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -1,3 +1,17 @@
+class puppet::master::preinstall {
+  augeas { "seed_fqdn_in_hosts_file" :
+    context => '/files/etc/hosts',
+    lens => 'Hosts.lns',
+    incl => '/etc/hosts',
+    changes => [
+      "set 01\ipaddr $::ipaddress",
+      "set 01\canonical $::fqdn",
+      "set 01\alias $::hostname",
+    ],
+    onlyif => "match *[ipaddr=\"$::ipaddress\"] size == 0",
+  }
+}
+
 class puppet::master::install {
   case $::operatingsystem {
     'Ubuntu' : {
@@ -11,6 +25,7 @@ class puppet::master::install {
   }
   package { $packages :
     ensure => present,
+    require => Class [ "puppet::master::preinstall" ],
   }
 }
 
