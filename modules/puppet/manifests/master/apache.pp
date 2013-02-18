ba02f889344dab6f7bf52bd2f7d303849d357f41
Bernd Weber <bernd@copperfroghosting.com>
2013-02-18 14:43:55 -0800

    Need vhost_name as well

--- modules/puppet/manifests/master/apache.pp
+++ modules/puppet/manifests/master/apache.pp
@@ -2,10 +2,11 @@
 class puppet::master::apache {
   require ( 'puppet::params' )
   apache::vhost { 'puppetmaster' :
-    priority => '1',
-    port     => $puppet::params::masterport,
-    template => 'puppet/puppetmaster.conf.erb',
-    docroot  => "${puppet::params::rackdir}/puppetmasterd/",
-    logroot  => $puppet::params::logdir,
+    priority   => '1',
+    vhost_name => "*",
+    port       => $puppet::params::masterport,
+    template   => 'puppet/puppetmaster.conf.erb',
+    docroot    => "${puppet::params::rackdir}/puppetmasterd/",
+    logroot    => $puppet::params::logdir,
   }
 }
