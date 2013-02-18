0d1d09b021f86e45bdc00bccf4499d347bc86c64
Bernd Weber <bernd@copperfroghosting.com>
2013-02-18 14:38:00 -0800

    Simplifying dependencies

--- modules/puppet/manifests/master/apache.pp
+++ modules/puppet/manifests/master/apache.pp
@@ -1,12 +1,11 @@
 
 class puppet::master::apache {
-  require ( "apache", 'puppet::params' )
+  require ( 'puppet::params' )
   apache::vhost { 'puppetmaster' :
     priority => '1',
     port     => puppet::params::masterport,
     template => 'puppet/puppetmaster.conf.erb',
     docroot  => "${puppet::params::rackdir}/puppetmasterd/",
     logroot  => $puppet::params::logdir,
-    require  => Class [ 'apache' ],
   }
 }
