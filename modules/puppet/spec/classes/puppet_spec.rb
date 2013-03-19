d330fe087dc140e113151e620a605c06c1e41d56
Bernd Weber <bernd@copperfroghosting.com>
2013-03-19 16:08:30 -0700

    Fixing whitespace

--- modules/puppet/spec/classes/puppet_spec.rb
+++ modules/puppet/spec/classes/puppet_spec.rb
@@ -32,11 +32,11 @@ describe 'puppet', :type => :class do
     }
     it 'should have a template with the correct contents' do
       content = catalogue.resource('file', '/etc/puppet/pupptet.conf').send(:parameters)[:content]
-      content.should match ("vardir = /var/lib/puppet")
-      content.should match ("logdir = /lor/log/puppet")
-      content.should match ("rundir = /var/run/puppet")
-      content.should match ("ssldir = /etc/puppet/ssl")
-      content.should match ("modulepath = /etc/puppet/modules")
+      content.should match("vardir = /var/lib/puppet")
+      content.should match("logdir = /lor/log/puppet")
+      content.should match("rundir = /var/run/puppet")
+      content.should match("ssldir = /etc/puppet/ssl")
+      content.should match("modulepath = /etc/puppet/modules")
     end  
       
     it { should contain_service("puppet").with(
