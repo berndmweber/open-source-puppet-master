d4660e068b94dcd303311e4602bc672785ede746
Bernd Weber <bernd@copperfroghosting.com>
2013-03-19 16:45:50 -0700

    Template matching finally works

--- modules/puppet/spec/classes/puppet_spec.rb
+++ modules/puppet/spec/classes/puppet_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'puppet', :type => :class do
-  context "On a Ubuntu 12.04 system" do
+  context 'On a Ubuntu 12.04 system' do
     let(:params) {{}}
     let(:node) { 'puppet.copperfroghosting.net' }
     let :facts do
@@ -16,30 +16,36 @@ describe 'puppet', :type => :class do
     it { should include_class('puppet::install') }
     it { should include_class('puppet::service') }
 
-    it { should contain_package("puppet") }
+    it { should contain_package('puppet') }
 
-    it { should contain_file("/etc/puppet").with(
+    it { should contain_file('/etc/puppet').with(
       'ensure'  => 'directory',
       'owner'   => 'puppet',
       'group'   => 'root',
       'require' => 'Class[Puppet::Install]'
       )
     }
-    it { should contain_file("/etc/puppet/puppet.conf").with(
+    it { should contain_file('/etc/puppet/puppet.conf').with(
       'ensure'  => 'file',
       'require' => 'File[/etc/puppet]'
       )
     }
+    let(:expected_lines) {
+      [ 'vardir = /var/lib/puppet',
+        'logdir = /var/log/puppet',
+        'rundir = /var/run/puppet',
+        'ssldir = /etc/puppet/ssl',
+        'modulepath = /etc/puppet/modules',
+      ]
+    }
     it 'should have a template with the correct contents' do
-      content = catalogue.resource('file', '/etc/puppet/pupptet.conf').send(:parameters)[:content]
-      content.should match("vardir = /var/lib/puppet")
-      content.should match("logdir = /lor/log/puppet")
-      content.should match("rundir = /var/run/puppet")
-      content.should match("ssldir = /etc/puppet/ssl")
-      content.should match("modulepath = /etc/puppet/modules")
+      content = subject.resource('file', '/etc/puppet/puppet.conf').send(:parameters)[:content]
+      expected_lines do |line|
+        content.should match(line)
+      end
     end  
       
-    it { should contain_service("puppet").with(
+    it { should contain_service('puppet').with(
       'ensure'     => 'running',
       'hasstatus'  => 'true',
       'hasrestart' => 'true',
