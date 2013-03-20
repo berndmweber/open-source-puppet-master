12e9bc5ae2faf507faae0c5a86b3558b85b1d0ab
Bernd Weber <bernd@copperfroghosting.com>
2013-03-20 15:34:06 -0700

    Making sure we cover all bases

--- modules/puppet/spec/classes/master_spec.rb
+++ modules/puppet/spec/classes/master_spec.rb
@@ -21,61 +21,98 @@ describe 'puppet::master', :type => :class do
     it { should include_class('puppet::configure') }
     it { should include_class('puppet::service') }
 
-    it { should contain_package('puppetmaster') }
+    context 'as simple puppetmaster' do
+      it { should contain_package('puppetmaster') }
+      it { should contain_service('puppetmaster').with(
+        'ensure'     => 'running',
+        'enable'     => 'true',
+        'require'    => 'Class[Puppet::Master::Install]'
+        )
+      }
+      it { should contain_file('/etc/puppet/fileserver.conf').with(
+        'ensure'  => 'file',
+        'require' => 'File[/etc/puppet]',
+        'notify'  => 'Service[puppetmaster]'
+        )
+      }
+      it { should contain_file('/etc/puppet/puppet.conf').with(
+        'ensure'  => 'file',
+        'require' => 'File[/etc/puppet]',
+        'notify'  => 'Service[puppetmaster]'
+        )
+      }
+    end
+    context 'as puppetmaster through passenger' do
+      let(:params) {
+        { :type => 'apache' }
+      }
 
-    it { should contain_file('/etc/puppet').with(
-      'ensure'  => 'directory',
-      'owner'   => 'puppet',
-      'group'   => 'root',
-      'require' => 'Class[Puppet::Install]'
-      )
-    }
-    it { should contain_file('/etc/puppet/puppet.conf').with(
-      'ensure'  => 'file',
-      'require' => 'File[/etc/puppet]'
-      )
-    }
-    it 'should have a file puppet.conf with the correct contents' do
-      verify_template(subject, '/etc/puppet/puppet.conf', [
-        'vardir = /var/lib/puppet',
-        'logdir = /var/log/puppet',
-        'rundir = /var/run/puppet',
-        'ssldir = /etc/puppet/ssl',
-        'modulepath = /etc/puppet/modules',
-        'user = puppet',
-        'group = puppet',
-        'archive_file_server = master.copperfroghosting.net',
-        'certname = master.copperfroghosting.net',
-        'server = master.copperfroghosting.net',
-        '\[master\]',
-        'certname = master.copperfroghosting.net',
-        'dns_alt_names = puppet,puppet.copperfroghosting.net,master,master.copperfroghosting.net',
-        '\[testing\]\n\s*modulepath = /etc/puppet/environments/testing/modules:/etc/puppet/modules\n\s*manifest = /etc/puppet/environments/testing/manifests/site.pp',
-        '\[development\]\n\s*modulepath = /etc/puppet/environments/development/modules:/etc/puppet/modules\n\s*manifest = /etc/puppet/environments/development/manifests/site.pp',
-      ])
-    end  
-    it { should contain_file('/etc/puppet/auth.conf').with(
-      'ensure'  => 'file',
-      'require' => 'File[/etc/puppet]'
-      )
-    }
-    it 'should have a file auth.conf with the correct contents' do
-      verify_template(subject, '/etc/puppet/puppet.conf', [
-        '# This file is controlled by puppet. Do NOT edit! #',
-      ])
-    end  
-    it { should contain_file('/etc/default/puppet').with(
-      'ensure'  => 'file',
-      'require' => 'Class[Puppet::Install]'
-      )
-    }
-    it { should contain_augeas('/etc/default/puppet').with(
-      'context' => '/files/etc/default/puppet',
-      'lens'    => 'Shellvars.lns',
-      'incl'    => '/etc/default/puppet',
-      'require' => 'File[/etc/default/puppet]'
-      )
-    }
+      it { should contain_package('puppetmaster-passenger') }
+      it { should_not contain_service('puppetmaster') }
+      it { should contain_file('/etc/puppet/fileserver.conf').with(
+        'ensure'  => 'file',
+        'require' => 'File[/etc/puppet]',
+        'notify'  => 'Service[httpd]'
+        )
+      }
+      it { should contain_file('/etc/puppet/puppet.conf').with(
+        'ensure'  => 'file',
+        'require' => 'File[/etc/puppet]',
+        'notify'  => 'Service[httpd]'
+        )
+      }
+    end
+
+    context 'puppet class now configures the system for the master setup' do
+      it { should contain_file('/etc/puppet').with(
+        'ensure'  => 'directory',
+        'owner'   => 'puppet',
+        'group'   => 'root',
+        'require' => 'Class[Puppet::Install]'
+        )
+      }
+      it 'should have a file puppet.conf with the correct contents' do
+        verify_template(subject, '/etc/puppet/puppet.conf', [
+          'vardir = /var/lib/puppet',
+          'logdir = /var/log/puppet',
+          'rundir = /var/run/puppet',
+          'ssldir = /etc/puppet/ssl',
+          'modulepath = /etc/puppet/modules',
+          'user = puppet',
+          'group = puppet',
+          'archive_file_server = master.copperfroghosting.net',
+          'certname = master.copperfroghosting.net',
+          'server = master.copperfroghosting.net',
+          '\[master\]',
+          'certname = master.copperfroghosting.net',
+          'dns_alt_names = puppet,puppet.copperfroghosting.net,master,master.copperfroghosting.net',
+          '\[testing\]\n\s*modulepath = /etc/puppet/environments/testing/modules:/etc/puppet/modules\n\s*manifest = /etc/puppet/environments/testing/manifests/site.pp',
+          '\[development\]\n\s*modulepath = /etc/puppet/environments/development/modules:/etc/puppet/modules\n\s*manifest = /etc/puppet/environments/development/manifests/site.pp',
+        ])
+      end  
+      it { should contain_file('/etc/puppet/auth.conf').with(
+        'ensure'  => 'file',
+        'require' => 'File[/etc/puppet]'
+        )
+      }
+      it 'should have a file auth.conf with the correct contents' do
+        verify_template(subject, '/etc/puppet/puppet.conf', [
+          '# This file is controlled by puppet. Do NOT edit! #',
+        ])
+      end  
+      it { should contain_file('/etc/default/puppet').with(
+        'ensure'  => 'file',
+        'require' => 'Class[Puppet::Install]'
+        )
+      }
+      it { should contain_augeas('/etc/default/puppet').with(
+        'context' => '/files/etc/default/puppet',
+        'lens'    => 'Shellvars.lns',
+        'incl'    => '/etc/default/puppet',
+        'require' => 'File[/etc/default/puppet]'
+        )
+      }
+    end
     # Needs fix in https://github.com/domcleal/rspec-puppet-augeas/issues/1 first
     # Commenting test out until fixed.
     #describe_augeas '/etc/default/puppet', :lens => 'Shellvars.lns', :target => '/etc/default/puppet' do
@@ -85,12 +122,6 @@ describe 'puppet::master', :type => :class do
     #    should execute.idempotently
     #  end
     #end
-    it { should contain_file('/etc/puppet/fileserver.conf').with(
-      'ensure'  => 'file',
-      'require' => 'File[/etc/puppet]',
-      'notify'  => 'Service[puppetmaster]'
-      )
-    }
     it 'should have a file fileserver.conf with the correct contents' do
       verify_template(subject, '/etc/puppet/fileserver.conf', [
         '# This file is controlled by puppet. Do NOT edit! #',
@@ -170,13 +201,6 @@ describe 'puppet::master', :type => :class do
     it { should contain_file('/etc/puppet/environments/development/manifests').with(
       'ensure'  => 'directory',
       'require' => 'File[/etc/puppet]'
-      )
-    }
-      
-    it { should contain_service('puppetmaster').with(
-      'ensure'     => 'running',
-      'enable'     => 'true',
-      'require'    => 'Class[Puppet::Master::Install]'
       )
     }
   end
