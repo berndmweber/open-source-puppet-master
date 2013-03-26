4b2fd0070e28cf66ab47896b72d23bdf377dd734
Bernd Weber <bernd@copperfroghosting.com>
2013-03-26 16:39:02 -0700

    Changing certificate detection

--- modules/puppet/manifests/master/apache.pp
+++ modules/puppet/manifests/master/apache.pp
@@ -60,9 +60,9 @@ class puppet::master::apache::configure {
     path      => '/bin:/sbin:/usr/bin:/usr/sbin',
     cwd       => $puppet::params::confdir,
     command   => 'puppet cert generate $(puppet master --configprint certname)',
-    unless    => 'puppet master --configprint hostcert',
+    unless    => "find ${puppet::params::ssldir}/certs -name ${::fqdn}.pem",
     logoutput => on_failure,
-    require   => Class [ 'puppet::master::install' ],
+    require   => Class [ 'puppet::master::configure' ],
   }
 
   apache::vhost { 'puppetmaster' :
