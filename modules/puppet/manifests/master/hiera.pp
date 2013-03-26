2ded74165edceb4a19cbaa62bdc56725d259fa05
Bernd Weber <bernd@copperfroghosting.com>
2013-03-26 11:52:42 -0700

    Adding missing files...

---
+++ modules/puppet/manifests/master/hiera.pp
@@ -0,0 +1,69 @@
+# == Class: puppet::master::hiera
+#
+# This is the Puppet Master class for the hiera configuration.
+#
+# === Examples
+#
+#  class { puppet::master::hiera : }
+#
+# === Authors
+#
+# Bernd Weber <mailto:bernd@copperfroghosting.com>
+#
+# === Copyright
+#
+# Copyright 2013 {Copper Frog LLC.}[copperfroghosting.com]
+#
+class puppet::master::hiera inherits puppet::params {
+  class { 'puppet::master::hiera::install' :
+    require => Class [ 'puppet::master' ],
+  }
+  class { 'puppet::master::hiera::configure' :
+    require => Class [ 'puppet::master::hiera::install' ],
+  }
+}
+
+# == Class: puppet::master::hiera::install
+#
+# This installs the hiera requirements.
+#
+# === Examples
+#
+#  class { puppet::master::hiera::install : }
+#
+class puppet::master::hiera::install {
+}
+
+# == Class: puppet::master::hiera::configure
+#
+# This configures hiera for puppet masters.
+#
+# === Examples
+#
+#  class { puppet::master::hiera::configure : }
+#
+class puppet::master::hiera::configure {
+  file { $puppet::params::hieraconf :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/puppet/hiera.yaml',
+    require => File [ $puppet::params::confdir ],
+  }
+  file { $puppet::params::hierapath :
+    ensure => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File [ $puppet::params::hieraconf ],
+  }
+  file { "${puppet::params::hierapath}/common.yaml" :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => "puppet:///modules/puppet/${puppet::params::hieradir}/common.yaml",
+    require => File [ $puppet::params::hierapath ],
+  }
+}
---
+++ modules/puppet/spec/classes/master_hiera_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe 'puppet::master::hiera', :type => :class do
+  let(:node) { 'master.copperfroghosting.net' }
+  let :fact_defaults do
+    {
+      :ipaddress   => '192.168.1.111',
+      :environment => 'production',
+      :domain      => 'copperfroghosting.net'
+    }
+  end
+  describe 'with operatingsystem specific facts' do
+    {
+      'Ubuntu-12.04' => {
+        :operatingsystem        => 'Ubuntu',
+        :operatingsystemrelease => '12.04'
+      },
+    }.each do |ossystem, osfacts|
+
+      describe "when operatingsystem is #{ossystem}" do
+        let :facts do
+          fact_defaults.merge(osfacts)
+        end
+        it { should include_class('puppet::params') }
+        it { should include_class('puppet::master::hiera::install') }
+        it { should include_class('puppet::master::hiera::configure') }
+        
+        it { should contain_file('/etc/puppet/hiera.yaml').with(
+          'ensure'  => 'file',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0755',
+          'require' => 'File[/etc/puppet]'
+          )
+        }
+        it { should contain_file('/etc/puppet/hieradata').with(
+          'ensure'  => 'directory',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0755',
+          'require' => 'File[/etc/puppet/hiera.yaml]'
+          )
+        }
+        it { should contain_file('/etc/puppet/hieradata/common.yaml').with(
+          'ensure'  => 'file',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0755',
+          'require' => 'File[/etc/puppet/hieradata]'
+          )
+        }
+      end
+    end
+  end
+end
