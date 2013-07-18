00d5c74a62f165de2b1fac482b9e672fb3ef0900
Bernd Weber <bernd@nvisionary.com>
2013-07-18 16:00:18 -0700

    Upgrading to passenger 4

--- modules/passenger/manifests/params.pp
+++ modules/passenger/manifests/params.pp
@@ -11,27 +11,36 @@
 # Sample Usage:
 #
 class passenger::params {
-  $passenger_version  = '3.0.9'
+  $passenger_version  = '4.0.10'
   $passenger_provider = 'gem'
+  
+  if $passenger_version > 4 {
+    $builddir = 'buildout'
+  } else {
+    $builddir = 'ext'
+  }
 
   case $osfamily {
     'debian': {
       $passenger_package      = 'passenger'
       $gem_path               = '/var/lib/gems/1.8/gems'
       $gem_binary_path        = '/var/lib/gems/1.8/bin'
-      $mod_passenger_location = "/var/lib/gems/1.8/gems/passenger-$passenger_version/ext/apache2/mod_passenger.so"
+      $passenger_root         = "/var/lib/gems/1.8/gems/passenger-${passenger_version}"
+      $mod_passenger_location = "${passenger_root}/${builddir}/apache2/mod_passenger.so"
     }
     'redhat': {
       $passenger_package      = 'passenger'
       $gem_path               = '/usr/lib/ruby/gems/1.8/gems'
       $gem_binary_path        = '/usr/lib/ruby/gems/1.8/gems/bin'
-      $mod_passenger_location = "/usr/lib/ruby/gems/1.8/gems/passenger-$passenger_version/ext/apache2/mod_passenger.so"
+      $passenger_root         = "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}"
+      $mod_passenger_location = "${passenger_root}/${builddir}/apache2/mod_passenger.so"
     }
     'darwin':{
       $passenger_package      = 'passenger'
       $gem_path               = '/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin'
       $gem_binary_path        = '/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin'
-      $mod_passenger_location = "/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/passenger-$passenger_version/ext/apache2/mod_passenger.so"
+      $passenger_root         = "/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/passenger-${passenger_version}"
+      $mod_passenger_location = "${passenger_root}/${builddir}/apache2/mod_passenger.so"
     }
     default: {
       fail("Operating system ${::operatingsystem} is not supported with the Passenger module")
