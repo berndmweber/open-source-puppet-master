f7fb0bea014de12fa4be79bd34c72b3fc006cb2b
Bernd Weber <bernd@nvisionary.com>
2013-08-19 11:12:03 -0700

    Updating to underlying version 0.1.0

--- modules/passenger/manifests/init.pp
+++ modules/passenger/manifests/init.pp
@@ -7,11 +7,17 @@
 #   [*passenger_version*]
 #     The Version of Passenger to be installed
 #
+#   [*passenger_ruby*]
+#     The path to ruby on your system
+#
 #   [*gem_path*]
 #     The path to rubygems on your system
 #
 #   [*gem_binary_path*]
 #     Path to Rubygems binaries on your system
+#
+#   [*passenger_root*]
+#     The passenger gem root directory
 #
 #   [*mod_passenger_location*]
 #     Path to Passenger's mod_passenger.so file
@@ -26,8 +32,10 @@
 #
 #  class { 'passenger':
 #    passenger_version      => '3.0.9',
+#    passenger_ruby         => '/usr/bin/ruby'
 #    gem_path               => '/var/lib/gems/1.8/gems',
 #    gem_binary_path        => '/var/lib/gems/1.8/bin',
+#    passenger_root         => '/var/lib/gems/1.8/gems/passenger-3.0.9'
 #    mod_passenger_location => '/var/lib/gems/1.8/gems/passenger-3.0.9/ext/apache2/mod_passenger.so',
 #    passenger_provider     => 'gem',
 #    passenger_package      => 'passenger',
@@ -40,8 +48,10 @@
 #
 class passenger (
   $passenger_version      = $passenger::params::passenger_version,
+  $passenger_ruby         = $passenger::params::passenger_ruby,
   $gem_path               = $passenger::params::gem_path,
   $gem_binary_path        = $passenger::params::gem_binary_path,
+  $passenger_root         = $passenger::params::passenger_root,
   $mod_passenger_location = $passenger::params::mod_passenger_location,
   $passenger_provider     = $passenger::params::passenger_provider,
   $passenger_package      = $passenger::params::passenger_package
@@ -51,48 +61,41 @@ class passenger (
   # NV change due to updated apache module
   require apache::dev
 
-  case $osfamily {
+  case $::osfamily {
     'debian': {
-      if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease >= 10.10 {
-        $pakages = [ 'libcurl4-openssl-dev' ]
-      } else {
-        $pakages = [ 'libopenssl-ruby', 'libcurl4-openssl-dev' ]
-      }
-      package { $pakages:
+      package { [$passenger::params::libruby, 'libcurl4-openssl-dev']:
         ensure => present,
         before => Exec['compile-passenger'],
+      }
+
+      File {
+        owner   => '0',
+        group   => '0',
+        notify  => Service['httpd'],
       }
 
       file { '/etc/apache2/mods-available/passenger.load':
         ensure  => present,
         content => template('passenger/passenger-load.erb'),
-        owner   => '0',
-        group   => '0',
         mode    => '0644',
       }
 
       file { '/etc/apache2/mods-available/passenger.conf':
         ensure  => present,
         content => template('passenger/passenger-enabled.erb'),
-        owner   => '0',
-        group   => '0',
         mode    => '0644',
       }
 
       file { '/etc/apache2/mods-enabled/passenger.load':
         ensure  => 'link',
         target  => '/etc/apache2/mods-available/passenger.load',
-        owner   => '0',
-        group   => '0',
         mode    => '0777',
-        require => File['/etc/apache2/mods-available/passenger.load'],
+        require => [ File['/etc/apache2/mods-available/passenger.load'], Exec['compile-passenger'], ],
       }
 
       file { '/etc/apache2/mods-enabled/passenger.conf':
         ensure  => 'link',
         target  => '/etc/apache2/mods-available/passenger.conf',
-        owner   => '0',
-        group   => '0',
         mode    => '0777',
         require => File['/etc/apache2/mods-available/passenger.conf'],
       }
@@ -123,7 +126,7 @@ class passenger (
   }
 
   exec {'compile-passenger':
-    path      => [ $gem_binary_path, '/usr/local/bin', '/usr/bin', '/bin'],
+    path      => [ $gem_binary_path, '/usr/bin', '/bin', '/usr/local/bin' ],
     command   => 'passenger-install-apache2-module -a',
     logoutput => on_failure,
     creates   => $mod_passenger_location,
--- modules/passenger/manifests/params.pp
+++ modules/passenger/manifests/params.pp
@@ -12,21 +12,31 @@
 #
 class passenger::params {
   $passenger_version  = '4.0.10'
+  $passenger_ruby     = '/usr/bin/ruby'
   $passenger_provider = 'gem'
   
   if versioncmp ($passenger_version, '4.0.0') > 0 {
-    $builddir = 'buildout'
+    $builddir     = 'buildout'
+    $is_version_4 = true
   } else {
-    $builddir = 'ext'
+    $builddir     = 'ext'
+    $is_version_4 = false
   }
 
-  case $osfamily {
+  case $::osfamily {
     'debian': {
       $passenger_package      = 'passenger'
       $gem_path               = '/var/lib/gems/1.8/gems'
       $gem_binary_path        = '/var/lib/gems/1.8/bin'
       $passenger_root         = "/var/lib/gems/1.8/gems/passenger-${passenger_version}"
       $mod_passenger_location = "${passenger_root}/${builddir}/apache2/mod_passenger.so"
+
+      # Ubuntu does not have libopenssl-ruby - it's packaged in libruby
+      if $::lsbdistid == 'Debian' and $::lsbmajdistrelease <= 5 {
+        $libruby              = 'libopenssl-ruby'
+      } else {
+        $libruby              = 'libruby'
+      }
     }
     'redhat': {
       $passenger_package      = 'passenger'
