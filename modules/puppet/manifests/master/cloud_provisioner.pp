1bab91907d07f6cd8dc36422571b89d7556cb81a
Bernd Weber <bernd@nvisionary.com>
2013-08-19 18:32:04 -0700

    More dependencies

--- modules/puppet/manifests/master/cloud_provisioner.pp
+++ modules/puppet/manifests/master/cloud_provisioner.pp
@@ -29,16 +29,24 @@ class puppet::master::cloud_provisioner inherits puppet::params {
 class puppet::master::cloud_provisioner::install {
   Package {
     ensure   => present,
-    provider => 'gem',
   }
+  package { [
+    'libxslt-dev',
+    'libxml2-dev',
+    ] : }
   package { 'nokogiri' :
     ensure => '1.4.4',
+    provider => 'gem',
+    require => Package [ 'libxml2-dev' ],
   }
   package { 'fog' :
     ensure  => '0.7.2',
+    provider => 'gem',
     require => Package [ 'nokogiri' ],
   }
-  package { 'guid' : }
+  package { 'guid' :
+    provider => 'gem',
+  }
 }
 
 # == Class: puppet::master::cloud_provisioner::configure
